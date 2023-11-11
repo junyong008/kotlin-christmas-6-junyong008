d965c44378197e3e6322c050a3d797e075458c04
junyong <junyong008@gmail.com>
2023-11-11 22:15:41 +0900

    feat(Orders): 빈 값 입력 예외 처리 기능 추가

--- src/main/kotlin/christmas/domain/Orders.kt
+++ src/main/kotlin/christmas/domain/Orders.kt
@@ -21,13 +21,17 @@ class Orders private constructor(private val orders: EnumMap<Menu, MenuCount>) {
         orders.entries.joinToString("\n") { (menu, menuCount) -> "$menu $menuCount" }
 
     companion object {
+
+        private const val DELIMITER_ORDERS = ","
+        private const val DELIMITER_ORDER = "-"
+
         fun fromString(input: String): Orders {
-            val inputOrders = input.split(",")
+            val inputOrders = input.split(DELIMITER_ORDERS)
             val outputOrders = EnumMap<Menu, MenuCount>(Menu::class.java)
             inputOrders.forEach {
-                val (menuTitle, menuCount) = it.split("-")
-                val menu = checkMenuExist(menuTitle)
-                val count = checkInteger(menuCount)
+                val (menuTitle, menuCount) = splitOrder(it)
+                val menu = getMenu(menuTitle)
+                val count = getCount(menuCount)
                 checkDuplicatedMenu(menu, outputOrders)
 
                 outputOrders[menu] = MenuCount(count)
@@ -35,16 +39,20 @@ class Orders private constructor(private val orders: EnumMap<Menu, MenuCount>) {
             return Orders(outputOrders)
         }
 
-        private fun checkMenuExist(menuTitle: String): Menu {
+        private fun splitOrder(order: String): List<String> {
+            require(order.contains(DELIMITER_ORDER)) { OrdersException.INVALID.message }
+            return order.split(DELIMITER_ORDER)
+        }
+
+        private fun getMenu(menuTitle: String): Menu {
             val menu = Menu.getMenuByTitle(menuTitle)
             return requireNotNull(menu) { OrdersException.INVALID.message }
         }
 
-        private fun checkInteger(input: String): Int =
+        private fun getCount(input: String): Int =
             requireNotNull(input.toIntOrNull()) { OrdersException.INVALID.message }
 
-        private fun checkDuplicatedMenu(menu: Menu, orders: EnumMap<Menu, MenuCount>) {
+        private fun checkDuplicatedMenu(menu: Menu, orders: EnumMap<Menu, MenuCount>) =
             require(!orders.containsKey(menu)) { OrdersException.INVALID.message }
-        }
     }
 }
