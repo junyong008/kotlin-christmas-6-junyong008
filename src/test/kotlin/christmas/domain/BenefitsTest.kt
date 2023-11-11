6160f63317fe8db40888023b2ba2ff7ba8255de0
junyong <junyong008@gmail.com>
2023-11-11 17:19:02 +0900

    test: 혜택 중 할인 금액만 계산 테스트 추가

--- src/test/kotlin/christmas/domain/BenefitsTest.kt
+++ src/test/kotlin/christmas/domain/BenefitsTest.kt
@@ -20,4 +20,21 @@ class BenefitsTest {
         // then
         assertEquals(Money(31246), result)
     }
+
+    @Test
+    fun `혜택 중 할인 금액만 계산`() {
+        // given
+        val benefits = Benefits(
+            listOf(
+                Benefit(BenefitType.DISCOUNT, "크리스마스 디데이", Money(1200)),
+                Benefit(BenefitType.DISCOUNT, "평일", Money(4046)),
+                Benefit(BenefitType.DISCOUNT, "특별", Money(1000)),
+                Benefit(BenefitType.EVENT, "증정", Money(25000))
+            )
+        )
+        // when
+        val result = benefits.getTotalDiscountAmount()
+        // then
+        assertEquals(Money(6246), result)
+    }
 }
