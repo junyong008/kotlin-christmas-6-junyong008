e6ed09219e5a033b733f48a4fa58baa51db6b436
junyong <junyong008@gmail.com>
2023-11-11 17:16:14 +0900

    feat: 총혜택 금액 계산 기능 추가

--- src/main/kotlin/christmas/domain/Benefit.kt
+++ src/main/kotlin/christmas/domain/Benefit.kt
@@ -3,7 +3,8 @@ package christmas.domain
 class Benefit(
     private val type: BenefitType,
     private val name: String,
-    private val amount: Money
+    private val amount: Money,
 ) {
+    fun getAmount() = amount
     override fun toString(): String = "$name $type: -$amount"
 }
--- src/main/kotlin/christmas/domain/Benefits.kt
+++ src/main/kotlin/christmas/domain/Benefits.kt
@@ -1,5 +1,8 @@
 package christmas.domain
 
 class Benefits(private val benefits: List<Benefit>) {
+
+    fun getTotalBenefitAmount(): Money =
+        benefits.fold(Money(0)) { acc, benefit -> acc.plus(benefit.getAmount()) }
     override fun toString(): String = benefits.joinToString("\n")
 }
